1445e56754440480d2f20a16a1de89bb45aa7909
Rasamizafy <rasamizafybryan98@gmail.com>
2020-07-20 17:35:46 +0200

    ok

--- app/delete_member.php
+++ app/delete_member.php
@@ -2,7 +2,7 @@
     session_start();
 
 	try {
-		$bdd = new PDO('mysql:host=eu-cdbr-east-02.cleardb.com;dbname=heroku_680327feb49fa0d;charset=utf8', 'bc15657febe502', 'b9a2f96d', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$bdd = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_680327feb49fa0d;charset=utf8', "bc15657febe502", "b9a2f96d", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	} catch (Exception $e) {
 		die('Erreur : ' . $e->getMessage());
     }
