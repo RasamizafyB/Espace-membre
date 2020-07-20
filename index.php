6b3c642668af2e63fa2776f2c7274b2797619d93
Rasamizafy <rasamizafybryan98@gmail.com>
2020-07-20 17:24:59 +0200

    ok

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	try {
-		$bdd = new PDO('mysql://bc15657febe502:b9a2f96d@us-cdbr-east-02.cleardb.com/heroku_680327feb49fa0d?reconnect=true', 'bc15657febe502', 'b9a2f96d@us', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$bdd = new PDO('mysql://bc15657febe502:b9a2f96d@us-cdbr-east-02.cleardb.com/heroku_680327feb49fa0d?reconnect=true', 'bc15657febe502', 'b9a2f96d', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	} catch (Exception $e) {
 		die('Erreur : ' . $e->getMessage());
 	}
